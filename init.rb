109120b55ffeb2793885ca04adfd4966d1b4998f
Eugene Hlyzov <hlyzov@gmail.com>
2009-11-01 14:58:11 +0600

    new handler

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
 require 'ostruct'
 require 'treetop'
 require 'tequila_jazz_handler'
-ActionView::Template.register_template_handler :jazz, TequilaJazzHandler
+ActionView::Template.register_template_handler :jazz, ActionView::TemplateHandlers::TequilaJazzHandler
--- lib/tequila_jazz_handler.rb
+++ lib/tequila_jazz_handler.rb
@@ -5,31 +5,19 @@ require 'tequila'
 require 'preprocessor'
 require 'tree'
 
-class TequilaJazzHandler < ActionView::TemplateHandler
-  # get view object ready to execute template code
-  def prepare_view(local_assigns)
-    @view.instance_eval do
-      # inject assigns into instance variables
-      assigns.each do |key, value|
-        instance_variable_set "@#{key}", value
-      end
-      # inject local assigns into reader methods
-      local_assigns.each do |key, value|
-        class << self; self; end.send(:define_method, key) { value }
+module ActionView
+  module TemplateHandlers
+    class TequilaJazzHandler < TemplateHandler
+      include Compilable
+
+      def compile(template)
+        <<CODE
+        controller.response.content_type ||= Mime:JSON
+        src = TequilaPreprocessor.run("#{template.source}")
+        self.output_buffer = TequilaParser.new.parse(src).eval(binding).build_hash.to_json
+CODE
       end
     end
-    def @view.get_binding
-      binding
-    end
-    @view.get_binding
-  end
-  def initialize(view)
-    @view = view
-  end
-  def render(template, local_assigns = {})
-    src = TequilaPreprocessor.run(template.source)
-    parser = TequilaParser.new
-    tree = parser.parse(src).eval(prepare_view(local_assigns))
-    tree.build_hash.to_json
   end
 end
+
