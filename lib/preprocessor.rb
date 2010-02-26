ba38830769a93246d36101e9640e96229a3d14a6
Ivan Nemytchenko <van.omsk@gmail.com>
2010-02-26 20:01:40 +0600

    very dirty commenting with "//" (comments only full strings from the beginning) // .wefwefwef  => aaa - works .wefwefwef  => aaa // does not work

--- lib/preprocessor.rb
+++ lib/preprocessor.rb
@@ -2,6 +2,7 @@ module TequilaPreprocessor
   EOB = "end\n".freeze
   EOC = "end\n".freeze
   JAZZ = ".jazz".freeze
+  
   def TequilaPreprocessor.run(source, offset = -1)
     source = replace(source)
     prev = offset
@@ -9,33 +10,36 @@ module TequilaPreprocessor
     code = nil
     lines = source.split("\n")
     for line in lines do
-      spaces, rest = spaces(line)
-      raise "#{spaces} spaces in line <#{line}>" if (spaces & 1 != 0)
-      tabs = spaces >> 1
-      if code
-        if tabs <= code
-          code = nil
-          text += EOC
-        else
-          text += "#{line}\n"
-          next
+      unless (line.strip =~ /\/\//) == 0
+        spaces, rest = spaces(line)
+        raise "#{spaces} spaces in line <#{line}>" if (spaces & 1 != 0)
+        tabs = spaces >> 1
+        if code
+          if tabs <= code
+            code = nil
+            text += EOC
+          else
+            text += "#{line}\n"
+            next
+          end
         end
+        case rest
+        when /^:code/
+          code = tabs
+        when /^(\+|\-|\<)/
+          diff = prev - tabs
+          diff += 1 if diff >= 0
+          diff.times { text += EOB }
+          prev = tabs
+        end
+        text += "#{line}\n"
       end
-      case rest
-      when /^:code/
-        code = tabs
-      when /^(\+|\-|\<)/
-        diff = prev - tabs
-        diff += 1 if diff >= 0
-        diff.times { text += EOB }
-        prev = tabs
-      end
-      text += "#{line}\n"
     end
     text += EOC if code
     (prev - offset).times { text += EOB }
     text
   end
+  
 private
   def TequilaPreprocessor.replace(source)
     text = ''
@@ -59,6 +63,7 @@ private
     f.close
     lines
   end
+  
   def TequilaPreprocessor.spaces(line)
     (/^ +/ =~ line) ? [$&.size, $'] : [0, line]
   end
